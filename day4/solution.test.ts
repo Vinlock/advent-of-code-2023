2b8362142bd9e0df128bb60b44efe1d289746e3c
Dak Washbrook <dak@gotrestle.com>
2023-12-03 22:25:40 -0800

    Day 4

--- day4/solution.test.ts
+++ day4/solution.test.ts
@@ -4,42 +4,47 @@ import solution from './solution'
 
 describe('Day 4', () => {
 	describe('Part 1', () => {
-		describe.concurrent('with test input', async () => {
+		describe('with test input', async () => {
 			const inputFile = await fs.readFile(`${__dirname}/test_input.txt`, 'utf8')
 
 			it('should equal 13', async () => {
 
-				const result = await solution(inputFile)
+				const result = solution(inputFile)
 
-				expect(result).toEqual(13)
+				expect(result.firstAnswer).toEqual(13)
 			})
 		})
 
-		describe.concurrent('with real input', () => {
-			it('should equal', async () => {
+		describe('with real input', () => {
+			it('should equal 22674', async () => {
 				const inputFile = await fs.readFile(`${__dirname}/input.txt`, 'utf8')
 
-				const result = await solution(inputFile)
-				console.log('result', result)
+				const result = solution(inputFile)
+
+				expect(result.firstAnswer).toEqual(22674)
 			})
 		})
 	})
 
-	// describe('Part 2', () => {
-	// 	describe.concurrent('with test input', () => {
-	// 		it('should equal', async () => {
-	// 			const inputFile = await fs.readFile(`${__dirname}/test_input.txt`, 'utf8')
-	//
-	// 			const result = await solution(inputFile)
-	// 		})
-	// 	})
-	//
-	// 	describe.concurrent('with real input', () => {
-	// 		it('should equal', async () => {
-	// 			const inputFile = await fs.readFile(`${__dirname}/input.txt`, 'utf8')
-	//
-	// 			const result = await solution(inputFile)
-	// 		})
-	// 	})
-	// })
+	describe('Part 2', () => {
+		describe('with test input', () => {
+			it('should equal 30', async () => {
+				const inputFile = await fs.readFile(`${__dirname}/test_input.txt`, 'utf8')
+
+				const result = solution(inputFile)
+
+				expect(result.secondAnswer).toEqual(30)
+			})
+		})
+
+		describe('with real input', () => {
+			it('should equal 5747443', async () => {
+				const inputFile = await fs.readFile(`${__dirname}/input.txt`, 'utf8')
+
+				const result = solution(inputFile)
+
+				expect(result.secondAnswer).toEqual(5747443)
+			})
+		})
+	})
 })
--- day4/solution.ts
+++ day4/solution.ts
@@ -1,27 +1,71 @@
-export default async (input: string) => {
+class Card {
+	public id: number
+	public numbersWon: string[] = []
+
+	constructor(
+		id: string,
+		public numbers: string[],
+		public winningNumbers: string[]
+	) {
+		this.id = Number(id)
+
+		for (const myNumber of this.numbers) {
+			for (const winningNumber of winningNumbers) {
+				if (myNumber === winningNumber) {
+					this.numbersWon.push(myNumber)
+				}
+			}
+		}
+	}
+}
+
+const numberOfCards = (card: Card, cardList: Card[]) => {
+	const numWinningNumbers = card.numbersWon.length
+	const cardId = Number(card.id)
+
+	let numCards = 1
+	for (let i = cardId + 1; i <= cardId + numWinningNumbers; i++) {
+		const foundCard = cardList.find((testCard) => {
+			return testCard.id === i
+		})
+
+		if (!foundCard) {
+			throw new Error(`Card ${i} not found`)
+		}
+
+		numCards = numCards + numberOfCards(foundCard, cardList)
+	}
+
+	return numCards
+}
+
+export default (input: string) => {
 	const lines = input.split('\n')
 
+
 	let firstAnswer = 0
-	const numOfWinningNumbers: Record<string, number> = {}
+	let secondAnswer = 0
+	const cardList: Card[] = []
+
 	for (const [lineNumber, line] of lines.entries()) {
 		if (!line) {
 			continue
 		}
 
 		const [cardInfo, numbers] = line.split(':')
-		const [_, cardNumber] = cardInfo.split(' ')
+		const [_, cardId] = cardInfo.split(' ').filter(t => t)
 		const [winningNumbers, myNumbers] = numbers.split(' | ')
 
 		const winningNumbersList = winningNumbers.split(' ').map(w => w.trim()).filter(t => t)
 		const myNumbersList = myNumbers.split(' ').map(m => m.trim()).filter(t => t)
 
+		const newCard = new Card(cardId, myNumbersList, winningNumbersList)
+		cardList.push(newCard)
+
 		let totalPoints = 0
 		for (const myNumber of myNumbersList) {
-			// console.log('myNumber', myNumber)
 			for (const winningNumber of winningNumbersList) {
-				// console.log('winningNumber', winningNumber)
 				if (winningNumber === myNumber) {
-					numOfWinningNumbers[cardNumber] += 1
 					if (totalPoints === 0) {
 						totalPoints = 1
 					} else {
@@ -33,8 +77,12 @@ export default async (input: string) => {
 		firstAnswer = totalPoints + firstAnswer
 	}
 
+	for (const card of cardList) {
+		secondAnswer = numberOfCards(card, cardList) + secondAnswer
+	}
+
 	return {
 		firstAnswer,
-		numOfWinningNumbers,
+		secondAnswer,
 	}
 }
